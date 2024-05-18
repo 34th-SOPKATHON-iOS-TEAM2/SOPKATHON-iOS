b865f2d18ebc6045ba97a7d35eb0290b13289f25
Chandrala <gudrms2538@gmail.com>
2024-05-19 04:03:03 +0900

    feat: 문제 뷰 UI 구현

---
+++ SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/ViewController/CustomButton.swift
@@ -0,0 +1,51 @@
+//
+//  CustomButton.swift
+//  SOPKATHON-iOS-TEAM2
+//
+//  Created by Chandrala on 5/19/24.
+//
+
+import UIKit
+
+final class CustomButton: UIButton {
+    var buttonHeight: CGFloat = 52
+    var buttonText: String?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    init(buttonText: String) {
+        super.init(frame: .zero)
+        self.buttonText = buttonText
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension CustomButton {
+    func setUI() {
+        setStyle()
+        setLayout()
+    }
+    
+    private func setStyle() {
+        self.setTitle(buttonText, for: .normal)
+        self.makeCornerRound(radius: buttonHeight/2)
+        self.backgroundColor = .white
+        self.titleLabel?.font = .font(.pretendardRegular, ofSize: 16)
+        self.setTitleColor(.black, for: .normal)
+    }
+    
+    private func setLayout() {
+        self.snp.makeConstraints {
+            $0.height.equalTo(buttonHeight)
+        }
+    }
+
+}
