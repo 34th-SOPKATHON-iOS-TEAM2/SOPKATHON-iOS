5fc619aa728199f4f761542408f1a979ff87d4da
youz2me <kynhun20@gachon.ac.kr>
2024-05-19 06:24:23 +0900

    feat: 결과 리스트 화면 구현 (#34)

--- SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/Application/SceneDelegate.swift
+++ SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/Application/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         self.window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: AnswerViewController())
+        let navigationController = UINavigationController(rootViewController: ArchiveViewController())
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
     }
