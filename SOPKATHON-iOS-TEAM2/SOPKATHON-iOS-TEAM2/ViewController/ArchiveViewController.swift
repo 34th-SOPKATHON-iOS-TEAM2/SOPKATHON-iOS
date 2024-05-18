621d262f5be4f9683a8b445a3476557f0a907cf0
youz2me <kynhun20@gachon.ac.kr>
2024-05-19 00:58:43 +0900

    feat: 기록 리스트 ViewController 추가 및 컴포넌트 세팅 (#12)

---
+++ SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/ViewController/ArchiveViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ArchiveViewController.swift
+//  SOPKATHON-iOS-TEAM2
+//
+//  Created by YOUJIM on 5/19/24.
+//
+
+import UIKit
+
+final class ArchiveViewController: UIViewController {
+    
+
+    private let scrollView: UIScrollView = UIScrollView()
+    
+    private let contentView: UIView = UIView()
+    
+    private let backgroundImageView: UIImageView = UIImageView().then {
+        // $0.image =
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    private let firstImageView: UIImageView = UIImageView().then {
+        // $0.image =
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    private let secondImageView: UIImageView = UIImageView().then {
+        // $0.image =
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    private let thirdImageView: UIImageView = UIImageView().then {
+        // $0.image =
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    private let fourthImageView: UIImageView = UIImageView().then {
+        // $0.image =
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    private let fifthImageView: UIImageView = UIImageView().then {
+        // $0.image =
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    private let sixthImageView: UIImageView = UIImageView().then {
+        // $0.image =
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    private let seventhImageView: UIImageView = UIImageView().then {
+        // $0.image =
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    private let eighthImageView: UIImageView = UIImageView().then {
+        // $0.image =
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    private let ninthImageView: UIImageView = UIImageView().then {
+        // $0.image =
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    private let tenthImageView: UIImageView = UIImageView().then {
+        // $0.image =
+        $0.contentMode = .scaleAspectFill
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setLayout()
+        setConstraint()
+    }
+    
+    private func setLayout() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        [
+            firstImageView,
+            secondImageView,
+            thirdImageView,
+            fourthImageView,
+            fifthImageView,
+            sixthImageView,
+            seventhImageView,
+            eighthImageView,
+            ninthImageView,
+            tenthImageView
+        ].forEach() {
+            contentView.addSubview($0)
+        }
+    }
+    
+    private func setConstraint() {
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.edges.width.equalToSuperview()
+        }
+    }
+}
