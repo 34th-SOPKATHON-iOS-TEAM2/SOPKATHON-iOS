920035f743fbf1009415e872b4853c47bb0f1af3
youz2me <kynhun20@gachon.ac.kr>
2024-05-19 03:17:42 +0900

    feat: 결과 화면 UI 구현 완료 (#14)

--- SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/ViewController/AnswerTableViewCell.swift
+++ SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/ViewController/AnswerTableViewCell.swift
@@ -10,34 +10,106 @@ import UIKit
 import Then
 
 
-class AnswerTableViewCell: UITableViewCell {
+final class AnswerTableViewCell: UITableViewCell {
     
     static let identifier = "answerTableViewCell"
     
-    private let problemLabel: UILabel = UILabel().then {
-        $0.text = "n번 문제"
-        // $0.font =
-        $0.textColor = .black
+    public let isCorrect: Bool = true
+    
+    private let backView: UIView = UIView().then {
+        $0.backgroundColor = UIColor(hexCode: "D9D9D9")
+        $0.layer.cornerRadius = 30
+        $0.clipsToBounds = true
     }
+    
+    private let wordLabel: UILabel = UILabel().then {
+        $0.font = UIFont.font(.pretendardRegular, ofSize: 20)
+        $0.text = "nn의 뜻은"
+    }
+    
+    private let statusImageView: UIImageView = UIImageView().then {
+        $0.image = .icnCheck
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private let answerLabel: UILabel = UILabel().then {
+        $0.font = UIFont.font(.pretendardBold, ofSize: 40)
+        $0.textColor = UIColor(hexCode: "0085FF")
+        $0.text = "mm"
+    }
+    
+    private let endLabel: UILabel = UILabel().then {
+        $0.font = UIFont.font(.pretendardRegular, ofSize: 20)
+        $0.textColor = .black
+        $0.text = "이다."
+    }
+    
+    private let chevronImageView: UIImageView = UIImageView().then {
+        $0.image = UIImage(systemName: "chevron.right")
+        $0.contentMode = .scaleAspectFit
+        $0.tintColor = .black
+    }
+    
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        setLayout()
+        setHierarchy()
+        setConstraint()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-
-        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 16, right: 0))
+    private func setHierarchy() {
+        self.selectionStyle = .none
+        self.backgroundColor = .clear
+        
+        self.contentView.addSubview(backView)
+        [
+            wordLabel,
+            statusImageView,
+            answerLabel,
+            endLabel,
+            chevronImageView
+        ].forEach { backView.addSubview($0) }
     }
     
-    private func setLayout() {
+    private func setConstraint() {
+        backView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(108)
+            $0.bottom.equalToSuperview().inset(16)
+        }
         
+        wordLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(20)
+            $0.leading.equalToSuperview().offset(36)
+        }
+        
+        statusImageView.snp.makeConstraints {
+            $0.top.equalTo(wordLabel.snp.bottom).offset(13)
+            $0.leading.equalToSuperview().offset(21)
+            $0.width.height.equalTo(32)
+        }
+        
+        answerLabel.snp.makeConstraints {
+            $0.centerY.equalTo(statusImageView)
+            $0.leading.equalTo(statusImageView.snp.trailing).offset(4)
+        }
+        
+        endLabel.snp.makeConstraints {
+            $0.bottom.equalTo(answerLabel.snp.bottom)
+            $0.leading.equalTo(answerLabel.snp.trailing).offset(8)
+            $0.bottom.equalToSuperview().inset(19)
+        }
+        
+        chevronImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(26)
+            $0.width.height.equalTo(24)
+        }
     }
 }
 
