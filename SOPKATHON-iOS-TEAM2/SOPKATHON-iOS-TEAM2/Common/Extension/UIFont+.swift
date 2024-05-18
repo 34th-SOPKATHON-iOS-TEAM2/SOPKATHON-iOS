4ab57bb403a8c87c746c3015d13dff0e14d84f15
youz2me <kynhun20@gachon.ac.kr>
2024-05-19 01:45:52 +0900

    chore: pretendard enum 케이스 추가 및 커스텀 폰트 extension 추가

---
+++ SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/Common/Extension/UIFont+.swift
@@ -0,0 +1,36 @@
+//
+//  UIFont+.swift
+//  SOPKATHON-iOS-TEAM2
+//
+//  Created by YOUJIM on 5/19/24.
+//
+
+import UIKit
+
+
+enum FontName: String {
+    case pretendardExtraBold = "Pretendard-ExtraBold"
+    case pretendardBold = "Pretendard-Bold"
+    case pretendardSemiBold = "Pretendard-SemiBold"
+    case pretendardMedium = "Pretendard-Medium"
+    case pretendardRegular = "Pretendard-Regular"
+    
+    case applegothicRegular = "AppleSDGothicNeo-Regular"
+    case applegothicThin = "AppleSDGothicNeo-Thin"
+    case applegothicLight = "AppleSDGothicNeo-Light"
+    case applegothicMedium = "AppleSDGothicNeo-Medium"
+    case applegothicSemibold = "AppleSDGothicNeo-SemiBold"
+    case applegothicBold = "AppleSDGothicNeo-Bold"
+}
+
+extension UIFont {
+    static func font(_ style: FontName, ofSize size: CGFloat) -> UIFont {
+        guard let customFont = UIFont(name: style.rawValue, size: size)
+        else {
+            print("Failed to load the \(style.rawValue) font. Return system font instead.")
+            return UIFont.systemFont(ofSize: size)
+        }
+        return customFont
+    }
+}
+
