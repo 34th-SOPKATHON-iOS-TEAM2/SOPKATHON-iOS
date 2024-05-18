1913957f2938889882d9f457266f3c7d4f6e7939
sozohoy <sozohoy@gmail.com>
2024-05-19 05:05:28 +0900

    feat: APITargets(#26)

--- SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/Network/API/APITarget+Attempts.swift
+++ SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/Network/API/APITarget+Attempts.swift
@@ -6,3 +6,77 @@
 //
 
 import Foundation
+
+import Moya
+
+extension APITarget {
+    enum Attempts {
+        case postCorrectInfo(DTO.PostCorrectInfoRequest)
+        case getCorrectInfo(DTO.GetCorrectInfoRequest)
+        case getRecordList(DTO.GetRecordListRequest)
+    }
+}
+
+extension APITarget.Attempts: TargetType {
+    var baseURL: URL {
+        return URL(string: Config.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .postCorrectInfo:
+            return "/attempts"
+        case .getCorrectInfo(let getCorrectInfoRequest):
+            return "/attempts"
+        case .getRecordList(let getRecordListRequest):
+            return "/attempts/logs"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .postCorrectInfo:
+            return .post
+        case .getCorrectInfo:
+            return .get
+        case .getRecordList:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        // 얘 헤더에 멤버 아이디 넣어야 함 1 넣을게 ~
+        case .postCorrectInfo(let postCorrectInfoRequest):
+            return .requestJSONEncodable(postCorrectInfoRequest)
+        case .getCorrectInfo(let getCorrectInfoRequest):
+            return .requestParameters(
+                parameters: ["date": getCorrectInfoRequest.date],
+                encoding: URLEncoding.default
+            )
+        case .getRecordList(let getRecordListRequest):
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .postCorrectInfo(let postCorrectInfoRequest):
+            return [
+                "Content-Type": "application/json",
+                "memberId": "1"
+            ]
+        case .getCorrectInfo(let getCorrectInfoRequest):
+            return [
+                "Content-Type": "application/json",
+                "memberId": "\(getCorrectInfoRequest.memberId)"
+            ]
+        case .getRecordList(let getRecordListRequest):
+            return [
+                "Content-Type": "application/json",
+                "memberId": "\(getRecordListRequest.memberId)"
+            ]
+        }
+    }
+    
+}
--- SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/Network/API/APITarget+Questions.swift
+++ SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/Network/API/APITarget+Questions.swift
@@ -6,3 +6,64 @@
 //
 
 import Foundation
+
+import Moya
+
+extension APITarget {
+    enum Questions {
+        case getQuestions
+        case getDetailQuestions(DTO.GetDetailQuestionRequest)
+        case getQuestionDescrption(DTO.GetQuestionDescriptionRequest)
+    }
+}
+
+extension APITarget.Questions: TargetType {
+    var baseURL: URL {
+        return URL(string: Config.baseURL)!
+    }
+
+    var path: String {
+        switch self {
+        case .getQuestions:
+            "/questions"
+        case .getDetailQuestions:
+            "/questions"
+        case .getQuestionDescrption(let getQuestionDescriptionRequest):
+            "/questions/\(getQuestionDescriptionRequest.questionId)/solution"
+        }
+
+    }
+
+    var method: Moya.Method {
+        return .get
+    }
+
+    var task: Moya.Task {
+        switch self {
+        case .getQuestions:
+            return .requestPlain
+        case .getDetailQuestions(let getDetailQuestionRequest):
+            return .requestParameters(
+                parameters: ["questionId": getDetailQuestionRequest.questionId],
+                encoding: URLEncoding.default
+            )
+        case .getQuestionDescrption:
+            return .requestPlain
+        }
+    }
+
+    var headers: [String : String]? {
+        switch self {
+        case .getQuestions:
+            return ["Content-Type": "application/json"]
+        case .getDetailQuestions(let getDetailQuestionRequest):
+            return ["Content-Type": "application/json"]
+        case .getQuestionDescrption(let getQuestionDescriptionRequest):
+            return [
+                "Content-Type": "application/json",
+                "memberId": "\(getQuestionDescriptionRequest.memberId)"
+            ]
+        }
+
+    }
+}
