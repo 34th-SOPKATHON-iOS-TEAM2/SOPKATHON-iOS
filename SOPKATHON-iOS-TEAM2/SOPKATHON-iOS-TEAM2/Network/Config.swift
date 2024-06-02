44743418b536d2749ca4d60c671ba8f6349881b6
Zoe <mam07065@naver.com>
2024-06-03 03:11:21 +0900

    add: Config에 Meta ID 추가

--- SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/Network/Config.swift
+++ SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/Network/Config.swift
@@ -11,6 +11,7 @@ enum Config {
     enum Keys {
         enum Plist {
             static let baseURL = "BASE_URL"
+            static let metaId = "Meta_id"
         }
     }
 
@@ -26,6 +27,12 @@ extension Config {
     static let baseURL: String = {
         guard let key = Config.infoDictionary[Keys.Plist.baseURL] as? String else {
             fatalError("Base URL is not set in plist for this configuration")
+        }
+        return key
+    }()
+    static let Meta_id: String = {
+        guard let key = Config.infoDictionary[Keys.Plist.metaId] as? String else {
+            fatalError("Meta ID is not set in plist for this configuration")
         }
         return key
     }()
