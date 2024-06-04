bf25c54fc61b91f1b7513ec3e16a8296d024ae0b
Zoe <mam07065@naver.com>
2024-06-05 02:37:30 +0900

    feat: service 계층(#47)

--- SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/Network/Service/AttemptService.swift
+++ SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/Network/Service/AttemptService.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+import Moya
+
+// MARK: Attempt
+class AttemptNetworkService: NetworkService<APITarget.Attempts> {
+    
+    func posetCorrectInfo(requestDTO: DTO.PostCorrectInfoRequest, completion: @escaping (Result<DTO.PostCorrectInfoResponse, Error>) -> Void) {
+        request(.postCorrectInfo(requestDTO), completion: completion)
+    }
+
+    
+    func getCorrectInfo(requestDTO: DTO.GetCorrectInfoRequest, completion: @escaping (Result<DTO.GetCorrectInfoResponse, Error>) -> Void) {
+        request(.getCorrectInfo(requestDTO),completion: completion)
+    }
+    
+    func getRecordList(requestDTO: DTO.GetRecordListRequest, completion: @escaping (Result<DTO.GetRecordListResponse, Error>) -> Void) {
+        request(.getRecordList(requestDTO), completion: completion)
+    }
+}
+
--- SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/Network/Service/QuestionService.swift
+++ SOPKATHON-iOS-TEAM2/SOPKATHON-iOS-TEAM2/Network/Service/QuestionService.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+import Moya
+
+// MARK: Qusetion
+class QuestionsNetworkService: NetworkService<APITarget.Questions> {
+    func getQuestions(completion: @escaping (Result<DTO.GetQuestionsResponse, Error>) -> Void) {
+        request(.getQuestions, completion: completion)
+    }
+
+    func getDetailQuestions(requestDTO: DTO.GetDetailQuestionRequest,
+                            completion: @escaping (Result<DTO.GetDetailQuestionResponse, Error>) -> Void) {
+        request(.getDetailQuestions(requestDTO), completion: completion)
+    }
+
+    func getQuestionDescription(requestDTO: DTO.GetQuestionDescriptionRequest, completion: @escaping (Result<DTO.GetQuestionDescriptionResponse, Error>) -> Void) {
+        request(.getQuestionDescrption(requestDTO), completion: completion)
+    }
+}
